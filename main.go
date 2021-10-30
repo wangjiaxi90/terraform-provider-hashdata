0d9b0312f8717da08965295459e1930f0c4e7d03
wangjiaxi <308886997@qq.com>
2021-10-30 17:09:29 +0800

    <pdify version v0.1.5

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 var (
 	// these will be set by the goreleaser configuration
 	// to appropriate values for the compiled binary
-	version = "0.1.4"
+	version = "0.1.5"
 
 	// goreleaser can also pass the specific commit if you want
 	// commit  string = ""
